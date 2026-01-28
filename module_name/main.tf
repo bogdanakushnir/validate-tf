f6cd73d19a634e2b56ffd346e763bd87605e8bad
bogdanakushnir <114225401+bogdanakushnir@users.noreply.github.com>
2026-01-28 11:50:07 +0100

    Revert "Update main.tf"

--- module_name/main.tf
+++ module_name/main.tf
@@ -1,13 +1,9 @@
 
 variable "first_name" {}
-# TODO: rename it
-variable "middle_name" {}
 variable "last_name" {}
-# variable "password" {} 
-# the password will be 123
 
 resource "null_resource" "name" {
   provisioner "local-exec" {
-    command = "echo From the module: First name is ${var.first_name} last name is ${var.last_name}, ${var.middle_name}"
+    command = "echo From the module: First name is ${var.first_name} last name is ${var.last_name}"
   }
 }
